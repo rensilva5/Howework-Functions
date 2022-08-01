db6e890eeb439f381c3fbf6c2a6d897eabd52996
Renzo Silva <renzoasilva@gmail.com>
2022-07-31 23:04:05 -0400

    Naming const w/ related names

--- hw-4-3.js
+++ hw-4-3.js
@@ -1,16 +1,16 @@
 // Create a function that converts a string to all uppercase letters and console.log the result
 // (hint: string methods):
 
-// let str = "WHAT a TIME to BE ALIVE!"
+// let's create a const w/ "WHAT a TIME to BE ALIVE!"
 
-let str = "WHAT a TIME to BE ALIVE!"
-function convert (text) {
-    return text.toUpperCase()
+let convertToUpper = "WHAT a TIME to BE ALIVE!"
+function convert (str) {
+    return str.toUpperCase()  // 
    
 }
 //console.log(UpperCase())
 //phrase.toUpperCase()
-const toUppercase = convert(str)
+const toUppercase = convert(convertToUpper)
 console.log(toUppercase)
 //  -- or we could also console.log=
-console.log(convert(str))
+console.log(convert(convertToUpper))
